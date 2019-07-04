447c2b3ff1144c91c621fb5a55d4a33400278e00
Darcy Webber <darcy@quantifish.co.nz>
2019-07-04 17:26:58 +1200

    updates to images and contact

--- js/index.js
+++ js/index.js
@@ -54,3 +54,28 @@
 
     };
 })(jQuery);
+
+
+/* Contact card */
+$(document).ready(function()
+{
+    $('.contact').click(function (e) 
+    {
+        $('.card').toggleClass('active');
+        $('.banner').toggleClass('active');
+        $('.photo').toggleClass('active');
+        $('.social-media-banner').toggleClass('active');
+        $('.email-form').toggleClass('active');  
+        var buttonText = $('button.contact#main-button').text();
+        if (buttonText === 'back')
+        {
+            buttonText = 'click to get in touch';
+            $('button.contact#main-button').text(buttonText);
+        }
+        else
+        {
+            buttonText = 'back';
+            $('button.contact#main-button').text(buttonText);
+        }
+    });
+});
